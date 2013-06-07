bab29d5e867459e2af24abfa062101663ec221dc
Yannick Van Avermaet <yannick.van.avermaet@persgroep.be>
2013-06-07 12:01:33 +0200

    autoload is in the wrong place

--- app/store/Places.js
+++ app/store/Places.js
@@ -8,14 +8,14 @@ Ext.define('llhApp.store.Places', {
 
     config: {
         model: 'llhApp.model.Place',
+		autoLoad: true,
         proxy: {
             type: 'jsonp',
             url: 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=B1QLWV5EFQN1I5ER3A15EDNVG3LFXOBJDKRVOHBGSTUCUGVT&client_secret=X3YXML0SSJ2FC13UGTVFGDO3OIHT1J4TGFOM55UO0V1EVWMM&v=20130606',
             reader: {
                 type: 'json',
 				rootProperty: 'response.venues'
-			},
-			autoLoad: true
+			}
 		}
 	}
 });
